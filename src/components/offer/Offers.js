f2e4ea50c8224c799930bab520bc9a6c80b40879
Izabelagit <i.zakrzewska@tutanota.com>
2022-01-25 18:08:44 +0100

    Changes made

--- src/components/offer/Offers.js
+++ src/components/offer/Offers.js
@@ -6,24 +6,15 @@ const Offers = ({ offers }) => {
     <section className="boxes">
       <h2 id="offer">Czym zajmuje się nasza firma?</h2>
       <div className="container-4">
-        <div className="special-offer">
-          <div className="dot"></div>
-          <div className="dot-item">
-            Usługa 1 <br />
-            (nowość)
-          </div>
-        </div>
-        /* {offers.map(() => return;)} */
-        <div className="item">Usługa 2</div>
-        <div className="item">Usługa 3</div>
-        <div className="item">Usługa 4</div>
-        <div className="item">Usługa 5</div>
-        <div className="item">Usługa 6</div>
+        {offers.map((item) => (
+          <OffersBox item={item} />
+        ))}
       </div>
     </section>
   );
 };
-/* Lista ofert powinna być generowana na podstawie tablicy w JS i funkcji .map
+
+/*Lista ofert powinna być generowana na podstawie tablicy w JS i funkcji .map
 
 Pojedynczy box oferty powinien być osobnym komponentem, który przyjmuje parametr 
 "isNew", ustawiający napis "nowość" oraz czerwoną kropkę zgodnie z jego wartością */
--- src/components/offer/OffersBox.js
+++ src/components/offer/OffersBox.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-const OffersBox = (isNew) => {
-  return;
-};
+const OffersBox = ({ item, id }) => {
+  if (item.isNew) {
+    return (
+      <div key={(item, id)} className="special-offer">
+        <div className="dot"></div>
+        <div className="dot-item">
+          {item.offer} <br />
+          (nowość)
+        </div>
+      </div>
+    );
+  }
 
+  return (
+    <div key={item.id} className="item">
+      {item.offer}
+    </div>
+  );
+};
 export default OffersBox;
